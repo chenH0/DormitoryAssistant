aad3e07f30b5980e2a0914dee2fa3efdc86bc2fe
chenH0 <326124418@qq.com>
2023-11-03 19:51:02 +0800

    增加member模块

---
+++ member/src/main/java/com/chen/assistant/member/config/MemberApplication.java
@@ -0,0 +1,16 @@
+package com.chen.assistant.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ComponentScan("com.chen")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
--- member/src/main/java/com/chen/assistant/member/controller/TestController.java
+++ member/src/main/java/com/chen/assistant/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.chen.dormitoryassistant;
+package com.chen.assistant.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello15234455455";
+        return "hello15234455455!!!!";
     }
 }
