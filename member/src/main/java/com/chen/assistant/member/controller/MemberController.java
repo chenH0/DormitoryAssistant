5297b267854970efd4482a293af49148e4675929
chenH0 <326124418@qq.com>
2023-11-06 20:21:43 +0800

    修改后端接口为@RequestBody,json传参，前后端联调

--- member/src/main/java/com/chen/assistant/member/controller/MemberController.java
+++ member/src/main/java/com/chen/assistant/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.chen.assistant.member.resp.MemberLoginResp;
 import com.chen.assistant.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -30,7 +27,7 @@ public class MemberController {
         return new CommonResp<>(register);
     }
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req){
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req){
         memberService.sendCode(req);
         return new CommonResp<>();
     }
