d6a1b3cc2348d18ec04dba875bc8f6114c5122fa
chenH0 <326124418@qq.com>
2023-11-08 16:13:35 +0800

    增加住户类型枚举

---
+++ member/src/main/java/com/chen/assistant/member/enums/HouseholdTypeEnum.java
@@ -0,0 +1,39 @@
+package com.chen.assistant.member.enums;
+
+public enum HouseholdTypeEnum {
+    BACHELOR("1", "本科生"),
+    MASTER("2", "硕士生"),
+    DOCTOR("3", "博士生");
+    private String code;
+    private String desc;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    HouseholdTypeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("HouseholdTypeEnum{");
+        sb.append("code='").append(code).append('\'');
+        sb.append(", desc='").append(desc).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
